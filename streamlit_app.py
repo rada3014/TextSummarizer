8db8f192dd9a5f148a28c2b096e081892378f4a9
Rahul Arora <31566616+rada3014@users.noreply.github.com>
2024-03-09 19:24:49 +0530

    Update streamlit_app.py
    taking model from Gdrive.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,26 @@
+
 import streamlit as st
-from transformers import pipeline
+import gdown
+import shutil
+from transformers import pipeline,T5ForConditionalGeneration,T5Tokenizer
+
+# Function to download the model from Google Drive
+def download_model_extract():
+    # Replace 'YOUR_FILE_ID' with the actual file ID from your Google Drive
+    gdown.download("https://drive.google.com/uc?export=download&id=1-Z0kbbOold1wMG1VhILcWEWrGBCBpOHj", "model1.zip", quiet=False)
+    shutil.unpack_archive('model1.zip', 'model1', 'zip')
+
+
+# Download the model if it's not already downloaded
+download_model_extract()
 
 def main():
     st.title("Text Summarization")
 
-    summarizer = pipeline(
-        task="summarization",
-        model="t5-small",
-        min_length=20,
-        max_length=40,
-        truncation=True,
-    ) 
+    # Load the T5-small model
+    model = T5ForConditionalGeneration.from_pretrained("model1")
+    tokenizer = T5Tokenizer.from_pretrained("t5-small")
+    summarizer = pipeline(task="summarization", model=model, tokenizer=tokenizer,min_length=20, max_length=40, truncation=True)
 
     # User input
     input_text = st.text_area("Enter the text you want to summarize:", height=200)
